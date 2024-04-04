6ad5873a2a11bd914c57dbf639710b17108da267
Joao <joaovictorsantos10@hotmail.com.br>
2024-04-04 14:56:11 -0300

    adicionado equestao 01

---
+++ Questao01.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Questao01 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+
+
+        in.close();
+    }
+}
