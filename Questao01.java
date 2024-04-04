dc1876ead392e810a5feee6ebef85a6c5c0a0e37
Joao <joaovictorsantos10@hotmail.com.br>
2024-04-04 15:13:58 -0300

    questao 01 resolvida

--- Questao01.java
+++ Questao01.java
@@ -3,7 +3,18 @@ import java.util.Scanner;
 public class Questao01 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        int[] num = new int[5];
 
+        for (int i = 0; i < num.length; i++) {
+            System.out.print("Insira o número " + (i+1) + ": ");
+            num[i] = in.nextInt();
+        }
+
+        System.out.print("\nNúmeros: ");
+
+        for (int numero : num) {
+            System.out.print(numero + " ");
+        }
 
 
         in.close();
