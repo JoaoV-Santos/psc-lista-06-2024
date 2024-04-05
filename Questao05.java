7f1d4f63b9f3ccd739e9d992b029edbeac3bb1b3
Joao <joaovictorsantos10@hotmail.com.br>
2024-04-04 21:05:35 -0300

    add questao 04

---
+++ Questao05.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Questao05 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+
+
+
+        in.close();
+    }
+}
